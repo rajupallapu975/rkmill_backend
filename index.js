cbd55ec35491a36466c504e5d8447e8054aa367a
rajupallapu975 <rajupallapu975@gmail.com>
2026-02-27 23:36:42 +0530

    chore: update port to 5000 and add docker configuration for AWS deployment

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ const startServer = async () => {
     startCronJobs();
 
     // 3. Start Express Server
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
